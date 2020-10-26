39f73ef4af5428fc5ec97045b5d17dc621a59eec
audric <audricperger@gmail.com>
2020-10-26 11:57:36 -0700

    touched up a bit

--- logic.js
+++ logic.js
@@ -24,7 +24,7 @@ function createFeatures(earthquakeData) {
     layer.bindPopup("<h3>" + feature.properties.title +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
       "<p>" + "Magnitude: " + feature.properties.mag + "<p>" +
-      "<p>" + "Depth: " + feature.geometry.coordinates[2] + "<p>"
+      "<p>" + "Depth (km): " + feature.geometry.coordinates[2] + "<p>"
       );
   }
 
